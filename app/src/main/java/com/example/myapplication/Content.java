600537c6aa8953e0deb3fd72e0dd12aa81b7e820
kentoy <kenalcantara55@gmail.com>
2024-01-22 22:43:01 +0800

    up

--- app/src/main/java/com/example/myapplication/Content.java
+++ app/src/main/java/com/example/myapplication/Content.java
@@ -1,14 +1,51 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Content extends AppCompatActivity {
 
+    BottomNavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content);
+
+
+
+        navigationView = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.body_container, new PlantsFragment()).commit();
+        navigationView.setSelectedItemId(R.id.menuPlant);
+        navigationView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+
+                switch (item.getItemId()){
+                    case R.id.menuPlant:
+                        fragment = new PlantsFragment();
+                        break;
+
+                    case R.id.menuScan:
+                        fragment = new CameraFragment();
+                        break;
+
+                    case R.id.menuHelp:
+                        fragment = new HelpFragment();
+                        break;
+
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.body_container, fragment).commit();
+
+
+
+            }
+        });
     }
 }
