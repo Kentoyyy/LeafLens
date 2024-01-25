5719ab6c562907213175c93af66e06a5b06da026
kentoy <kenalcantara55@gmail.com>
2024-01-25 16:05:36 +0800

    ups

--- app/src/main/java/com/example/myapplication/PlantsFragment.java
+++ app/src/main/java/com/example/myapplication/PlantsFragment.java
@@ -2,8 +2,10 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,16 +15,42 @@ import android.widget.ImageView;
 
 public class PlantsFragment extends Fragment {
 
-    ImageView btnaloe,btnTulsi,btnWildTea;
+    ImageView btnaloe,btntulsi,btnwildtea;
+
+    SearchView searchView;
+    RecyclerView recyclerView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_plants, container, false);
 
+        recyclerView = view.findViewById(R.id.recycleView);
+        searchView = view.findViewById(R.id.recycleView);
+
         btnaloe = view.findViewById(R.id.buttonAloevera);
-        btnTulsi = view.findViewById(R.id.btnTulsi);
-        btnWildTea = view.findViewById(R.id.btnWildTea);
+
+        btntulsi = view.findViewById(R.id.btnTulsi);
+
+        btnwildtea = view.findViewById(R.id.btnWildTea);
+
+        btnwildtea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment plantcontent = new WildTeaFragment();
+                FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.body_container, plantcontent).commit();
+            }
+        });
+
+        btntulsi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment plantcontent = new TulsiFragment();
+                FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.body_container, plantcontent).commit();
+            }
+        });
 
 
         btnaloe.setOnClickListener(new View.OnClickListener() {
@@ -34,23 +62,7 @@ public class PlantsFragment extends Fragment {
             }
         });
 
-        btnTulsi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Fragment plantcontent = new TulsiFragment();
-                FragmentTransaction tulsi = getActivity().getSupportFragmentManager().beginTransaction();
-                tulsi.replace(R.id.body_container, plantcontent).commit();
-            }
-        });
 
-        btnTulsi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Fragment plantcontent = new WildTeaFragment();
-                FragmentTransaction wildtea = getActivity().getSupportFragmentManager().beginTransaction();
-                wildtea.replace(R.id.body_container, plantcontent).commit();
-            }
-        });
         return view;
 
     }
