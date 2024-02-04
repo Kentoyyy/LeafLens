d30e545b938da9cb8ef9f776ddb666bde58dd007
kentoy <kenalcantara55@gmail.com>
2024-02-04 17:10:11 +0800

    plantcare tips done & new logo of splash

--- app/src/main/java/com/example/myapplication/PlantTipsFragment.java
+++ app/src/main/java/com/example/myapplication/PlantTipsFragment.java
@@ -3,15 +3,17 @@ package com.example.myapplication;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class PlantTipsFragment extends Fragment {
 
-
+    Button btnback;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -19,7 +21,16 @@ public class PlantTipsFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_plant_tips, container, false);
 
+        btnback = view.findViewById(R.id.btnBacktips);
 
+        btnback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment backtips = new HomeFragment();
+                FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.body_container, backtips).commit();
+            }
+        });
 
         return view;
     }
